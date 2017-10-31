2ee2d05c0830a83db9fe8f23500211483b57d525
brianlan <brianlanbo@gmail.com>
2017-10-31 12:20:46 +0800

    first commit

---
+++ download.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]
+  then
+    echo "Invalid number of arguments. Usage: download.sh <python_package_name>"
+    exit 1
+fi
+
+PACKAGE=$1; shift;
+
+echo "start to build docker image"
+docker build -t tmp_img .
+
+echo "start to download python packages and then put them into a tar file"
+docker run --rm -v tmp_vol:/__packages__ -v $(pwd):/__tmp__ tmp_img bash -c "pip install --cache-dir /__packages__ ${PACKAGE} ; tar cvf /__tmp__/py_archive.tar /__packages__"
+
+echo "deleting temp docker volume"
+docker volume rm tmp_vol
+
+echo "deleting temp docker image"
+docker rmi tmp_img
