1d0bda4cdd068b7f8f7360c2938c24720ad1d4ff
brianlan <brianlanbo@gmail.com>
2017-11-17 16:50:39 +0800

    added in_china mode

--- download.sh
+++ download.sh
@@ -2,23 +2,31 @@
 
 ARCHIVE_NAME="pip_archive"
 
-if [ $# -ne 2 ]
+if [ $# -lt 2 ]
   then
-    echo "Invalid number of arguments. Usage: download.sh <python_package_name> <python_version>(2|2.7|2.7.14|3|3.5.2|3.6|...)"
+    echo "Invalid number of arguments. Usage: download.sh <python_package_name> <python_version>(2|2.7|2.7.14|3|3.5.2|3.6|...) [in_china]"
     exit 1
 fi
 
 PACKAGE=$1; shift;
 PY_VER=$1; shift;
+IN_CHINA=$1; shift;
 
 echo "=== start to build docker image ==="
 docker build -t tmp_img --build-arg python_version=${PY_VER} .
+
+if [ "$IN_CHINA" == "in_china" ]
+  then
+    douban_pypi="-i https://pypi.douban.com/simple"
+  else
+    douban_pypi=""
+fi
 
 if [ $? -eq 0 ]
   then
     echo "=== start to download python packages and then put them into a tar.gz file ==="
     docker run --rm -v tmp_vol:/${ARCHIVE_NAME} -v $(pwd):/__tmp__ tmp_img \
-      bash -c "pip download ${PACKAGE} --dest /${ARCHIVE_NAME}; tar czvf /__tmp__/${ARCHIVE_NAME}.tar.gz /${ARCHIVE_NAME}"
+      bash -c "pip download ${douban_pypi} ${PACKAGE} --dest /${ARCHIVE_NAME}; tar czvf /__tmp__/${ARCHIVE_NAME}.tar.gz /${ARCHIVE_NAME}"
 
     echo "=== generate install.sh ==="
     echo "tar zxvf ./${ARCHIVE_NAME}.tar.gz" > $(pwd)/install.sh
